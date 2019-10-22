e43d5c77cf47d50861762ce63ad545f61972c00a
Mahmudur Rahman Hera <mahmudhera93@gmail.com>
2019-10-22 16:28:17 +0600

    Add files via upload

---
+++ simulator.cpp
@@ -0,0 +1,344 @@
+#include "simulator.h"
+
+Simulator::Simulator(int N) {
+
+    this->N = N;
+    eventQueue =  new priority_queue <Event*, vector <Event*>, CompareEvent> ();
+
+    Q = new deque<Customer*>*[N];
+    for(int i = 0; i < N; i++)
+        Q[i] = new deque<Customer*>();
+
+    serverBusy = new bool[N];
+    customerBeingServed = new Customer*[N];
+    for(int i = 0; i < N; i++) {
+        serverBusy[i] = false;
+        customerBeingServed[i] = NULL;
+    }
+
+    customerIdRecord = 0;
+
+    serviceTimeStream = new RandomStream(SERVICE_TIME_MEAN);
+    interArrivalTimeStream = new RandomStream(INTER_ARRIVAL_TIME_MEAN);
+
+}
+
+
+t_simtime Simulator::now() {
+    return this->simclock;
+}
+
+
+int Simulator::getFreeTellerId() {
+    for(int i = 0; i < N; i++)
+        if(serverBusy[i] == false && Q[i]->empty())
+            return i;
+    return -1;
+}
+
+
+int Simulator::getShortestQuedTellerId() {
+    int tellerId_ = 0;
+    int leastCustomerCount = Q[0]->size();
+    for(int i = 1; i < N; i++) {
+        if(Q[i]->size() < leastCustomerCount) {
+            leastCustomerCount = Q[i]->size();
+            tellerId_ = i;
+        }
+    }
+    return tellerId_;
+}
+
+
+int Simulator::getJockeyableQueueId(int tellerId) {
+
+    int jumper = -1;
+    int minDistance = N * N;
+    int totalSizeOfThisTeller = Q[tellerId]->size();
+    serverBusy[tellerId] ? totalSizeOfThisTeller++ : totalSizeOfThisTeller += 0;
+
+    for(int otherTellerId = 0; otherTellerId < N; otherTellerId++) {
+
+        if(otherTellerId == tellerId)
+            continue;
+
+        int totalSizeOfOtherTeller = Q[otherTellerId]->size();
+        serverBusy[otherTellerId] ? totalSizeOfOtherTeller++ : totalSizeOfOtherTeller += 0;
+        int distance = abs(tellerId - otherTellerId);
+
+        if(otherTellerId != tellerId && distance < minDistance && totalSizeOfOtherTeller > totalSizeOfThisTeller + 1) {
+            jumper = otherTellerId;
+            minDistance = distance;
+        }
+
+    }
+
+    return jumper;
+
+}
+
+
+void Simulator::jockey(int tellerId) {
+
+    /** determine if there is customer to jockey. if no, then return immediately */
+    int jumperJockeysFrom = getJockeyableQueueId(tellerId);
+    if(jumperJockeysFrom == -1) {
+        return;
+    }
+
+    /** now, there is someone to jockey. remove this customer from the tail, add to this Q */
+    Customer *C = Q[jumperJockeysFrom]->back();
+    Q[jumperJockeysFrom]->pop_back();
+    timeAvg[jumperJockeysFrom]->pushData(Q[jumperJockeysFrom]->size(), now());
+    addCustomerToQueue(C, tellerId);
+
+    /** this customer will jockey to "tellerId". determine if the teller with
+        "tellerId" is free. if so, then add customer to service by calling
+        make server busy. else, put the customer at the end of the queue */
+    bool thisTellerBusy = this->serverBusy[tellerId];
+    if(!thisTellerBusy) {
+        makeServerBusy(tellerId);
+        t_simtime departureTime = now() + serviceTimeStream->next();
+        DepartureEvent *e = new DepartureEvent(departureTime, tellerId);
+        scheduleEvent(e);
+    }
+
+}
+
+
+void Simulator::scheduleEvent(Event *event) {
+    this->eventQueue->push(event);
+}
+
+
+void Simulator::run() {
+
+    while (eventQueue->empty() == false) {
+
+        Event *event = this->eventQueue->top();
+        this->eventQueue->pop();
+
+        if (event == NULL)
+            break;
+
+        // cout << (*event) << endl;
+
+        this->simclock = event->getEventTime();
+        event->processEvent(this);
+        delete event;
+
+    }
+
+}
+
+
+void Simulator::setSimulationEndTime(t_simtime endTime) {
+    Event *endEvent = new EndEvent(endTime);
+    this->scheduleEvent(endEvent);
+    this->serviceEndsAt = endTime;
+}
+
+
+Simulator::~Simulator() {
+    if (eventQueue) {
+        while (!eventQueue->empty()) {
+            Event *event = eventQueue->top();
+            eventQueue->pop();
+            delete event;
+        }
+        delete eventQueue;
+    }
+    for(int i = 0; i < N; i++) {
+        if(Q[i]) {
+            while(!Q[i]->empty()) {
+                Customer *C = Q[i]->front();
+                Q[i]->pop_front();
+                timeAvg[i]->pushData(Q[i]->size(), now());
+                delete C;
+            }
+            delete Q[i];
+        }
+    }
+    delete Q;
+    delete serverBusy;
+    for(int i = 0; i < N; i++) {
+        if(this->customerBeingServed[i]) {
+            delete customerBeingServed[i];
+        }
+    }
+    delete customerBeingServed;
+    delete serviceTimeStream;
+    delete interArrivalTimeStream;
+
+}
+
+
+void Simulator::makeServerBusy(int tellerId) {
+    if(serverBusy[tellerId])
+        return;
+    if(Q[tellerId]->empty()) {
+        return;
+    }
+    Customer *customer = Q[tellerId]->front();
+    Q[tellerId]->pop_front();
+    timeAvg[tellerId]->pushData(Q[tellerId]->size(), now());
+    customerBeingServed[tellerId] = customer;
+    customerBeingServed[tellerId]->serviceTimeStartsAt = now();
+    serverBusy[tellerId] = true;
+}
+
+
+void Simulator::makeServerIdle(int tellerId) {
+    if(!serverBusy[tellerId] || customerBeingServed[tellerId] == NULL) {
+        return;
+    }
+    serverBusy[tellerId] = false;
+    customerBeingServed[tellerId]->departureTime = this->now();
+    delete customerBeingServed[tellerId];
+    customerBeingServed[tellerId] = NULL;
+}
+
+// Event classes
+
+EndEvent::EndEvent(t_simtime time, EventType type, string name): Event (time, type, name) { }
+EndEvent::EndEvent(t_simtime time): Event (time, EXIT, string("EXIT")) { }
+void EndEvent::processEvent(Simulator *sim) { }
+
+
+ArrivalEvent::ArrivalEvent(t_simtime time, EventType type, string name): Event (time, type, name) { }
+ArrivalEvent::ArrivalEvent(t_simtime time): Event (time, ARRIVAL, string("ARRIVAL")) { }
+
+void ArrivalEvent::processEvent(Simulator *sim) {
+
+    /*** first schedule the next arrival */
+    t_simtime nextArrivalTime = sim->now() + sim->interArrivalTimeStream->next();
+    if(nextArrivalTime <= sim->serviceEndTime()) {
+        ArrivalEvent *event = new ArrivalEvent(nextArrivalTime);
+        sim->scheduleEvent(event);
+    }
+
+    /** construct a customer object */
+    Customer *C = new Customer(sim->nextCustomerId(), sim->now());
+
+    /** is a teller idle? */
+    int idleTellerId = sim->getFreeTellerId();
+
+    /** if idle found, then add the customer to this Q, and immediately call makeServerBusy()
+        so that this customer is added to the service. After that schedule a departure */
+    if(idleTellerId != -1) {
+
+        sim->addCustomerToQueue(C, idleTellerId);
+        sim->makeServerBusy(idleTellerId);
+
+        t_simtime departureTime = sim->now() + sim->serviceTimeStream->next();
+        DepartureEvent *e = new DepartureEvent(departureTime, idleTellerId);
+        sim->scheduleEvent(e);
+
+    }
+
+    /** else, no idle is found. then add this customer C to the queue with shortest length */
+    else {
+        int shortestQuedTellerID = sim->getShortestQuedTellerId();
+        sim->addCustomerToQueue(C, shortestQuedTellerID);
+    }
+
+}
+
+
+DepartureEvent::DepartureEvent(t_simtime time, int tellerId, EventType type, string name) : Event (time, type, name) { this->tellerId = tellerId; }
+DepartureEvent::DepartureEvent(t_simtime time, int tellerId) : Event(time, DEPARTURE, string("DEPARTURE")) { this->tellerId = tellerId; }
+
+void DepartureEvent::processEvent(Simulator *sim) {
+
+    //logger << "dept from teller " << tellerId << " at " << sim->now() << endl;
+
+    /*** make the server idle (make the previous customer leave), and then try to make server busy.
+         if there is someone at the queue, then the customer from front will be automatically sent
+         to the teller for service */
+
+    sim->makeServerIdle(tellerId);
+    sim->makeServerBusy(tellerId);
+
+    /*** now determine if the server (teller) is actually busy, then someone from Q has just entered.
+         So schedule his departure */
+
+    if(sim->isServerBusy(tellerId)) {
+        t_simtime departureTime = sim->now() + sim->serviceTimeStream->next();
+        DepartureEvent *event = new DepartureEvent(departureTime, tellerId);
+        sim->scheduleEvent(event);
+    }
+
+    /*** invoke the jockey routine. this is a non-event routine. jockeying is handled separately in this
+         separate method. */
+
+    sim->jockey(tellerId);
+
+}
+
+
+int main(int argc, char **argv) {
+
+    srand(time(NULL));
+
+    result << "Multi-teller bank with jockeying " << endl;
+    result << "________________________________________________________________________________" << endl;
+    result << "Total tellers:\t\t\t\t2 to 8" << endl;
+    result << "Mean inter-arrival time:\t\t" << INTER_ARRIVAL_TIME_MEAN << " minutes" << endl;
+    result << "Mean service time:\t\t\t" << SERVICE_TIME_MEAN << " minutes" << endl;
+    result << "Duration:\t\t\t\t8 hours" << endl;
+
+    result << endl << endl;
+
+    result << "#tellers\tAvg q len\tAvg delay\tLeft boundary\tRight boundary" << endl;
+    result << "________________________________________________________________________________" << endl;
+
+    for(int numTellers = 4; numTellers <= 9; numTellers++) {
+
+        double accumulatedAverageDelay = 0;
+        double accumulatedQueueLength = 0;
+        double accumulatedRange = 0;
+
+        for(int i = 0; i < RUN_SIMULATION_TIMES; i++) {
+
+            int N = numTellers;
+
+            Simulator* sim = new Simulator(N);
+            timeAvg = new TimeAvgGenerator*[N];
+            for(int i = 0; i < N; i++)
+                timeAvg[i] = new TimeAvgGenerator();
+            avg = new AvgGenerator();
+
+            sim->scheduleEvent(new ArrivalEvent(0.0));
+            sim->setSimulationEndTime(480.0);
+            sim->run();
+
+            delete sim;
+
+            double timeAverage = 0;
+            for(int i = 0; i < N; i++) {
+                timeAverage += timeAvg[i]->timeAvg();
+            }
+
+            accumulatedAverageDelay += avg->avg();
+            accumulatedQueueLength += timeAverage;
+            accumulatedRange += avg->getConfidenceIntervalRange();
+
+            delete avg;
+            for(int i = 0; i < N; i++)
+                delete timeAvg[i];
+            delete timeAvg;
+
+        }
+
+        double averageAverageDelay = accumulatedAverageDelay / RUN_SIMULATION_TIMES;
+        double averageRange = accumulatedRange / RUN_SIMULATION_TIMES;
+        double leftBoundary = averageAverageDelay - averageRange;
+        double rightBoundary = averageAverageDelay + averageRange;
+        double averageQueueLength = accumulatedQueueLength / RUN_SIMULATION_TIMES;
+
+        printf("%d\t\t%-10.6f\t%-10.6f\t%-10.6f\t%-10.6f\n", numTellers, averageQueueLength, averageAverageDelay, leftBoundary, rightBoundary);
+    }
+
+    return 0;
+
+}
---
+++ simulator.h
@@ -0,0 +1,281 @@
+#include <queue>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <random>
+#include <ctime>
+#include <deque>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+#define INVALID_TIME             -1.0
+#define INTER_ARRIVAL_TIME_MEAN  1.0
+#define SERVICE_TIME_MEAN        4.5
+#define INFINITY                 99999999
+#define RUN_SIMULATION_TIMES     100
+
+#define result cout
+
+class TimeAvgGenerator;
+class AvgGenerator;
+
+TimeAvgGenerator **timeAvg = NULL;
+AvgGenerator     *avg      = NULL;
+
+typedef double t_simtime;
+
+class Simulator;
+
+enum EventType {
+    EXIT,
+    ARRIVAL,
+    DEPARTURE
+};
+
+class Event {
+    private:
+        t_simtime eventTime;
+        EventType eventType;
+        string eventName;
+
+    public:
+        Event(t_simtime time, EventType type, string name) {
+            this->eventTime = time;
+            this->eventType = type;
+            this->eventName = name;
+        }
+
+        inline Event(t_simtime time)    { this->eventTime = time; }
+
+        inline t_simtime getEventTime() { return this->eventTime; }
+        inline EventType getEventType() { return this->eventType; }
+        inline string getEventName ()   { return this->eventName; }
+
+        friend ostream & operator << (ostream & out, Event &e);
+
+        virtual void processEvent(Simulator *sim) = 0;
+};
+
+
+ostream & operator << (ostream & out, Event & e) {
+    out << e.getEventName() << " at " << e.getEventTime() ;
+    return out;
+}
+
+
+/** Comparing two events: event with lower time goes first */
+class CompareEvent {
+    public:
+        bool operator() (Event* &e1, Event* &e2) {
+            return e1->getEventTime() > e2->getEventTime();
+        }
+};
+
+
+/** stats generators */
+
+class TimeAvgGenerator {
+
+        double accumulatedValue;
+        double lastRecordedTime;
+        int    lastRecordedValue;
+        int    totalSamples;
+        double maximum;
+        double minimum;
+
+    public:
+        TimeAvgGenerator() {
+            accumulatedValue = 0.0;
+            lastRecordedValue = 0.0;
+            lastRecordedTime = 0.0;
+            totalSamples = 0;
+            maximum = -INFINITY;
+            minimum = +INFINITY;
+        }
+
+        void pushData(int length, t_simtime timeOfRecord) {
+            accumulatedValue += (timeOfRecord - lastRecordedTime) * lastRecordedValue;
+            lastRecordedTime = timeOfRecord;
+            lastRecordedValue = length;
+            totalSamples++;
+            if(length < minimum)
+                minimum = length;
+            if(length > maximum)
+                maximum = length;
+        }
+
+        double timeAvg() {
+            return accumulatedValue / lastRecordedTime;
+        }
+
+        int getSampleCount() {
+            return totalSamples;
+        }
+
+        double getMaxValue() {
+            return maximum;
+        }
+
+        double getMinValue() {
+            return minimum;
+        }
+
+};
+
+
+
+class AvgGenerator {
+
+        t_simtime accumulatedValue;
+        t_simtime valueRecords[1000];
+        int noOfSamples;
+        double maximum;
+        double minimum;
+
+    public:
+        AvgGenerator() {
+            accumulatedValue = 0;
+            noOfSamples = 0;
+            maximum = -INFINITY;
+            minimum = +INFINITY;
+        }
+
+        void pushData(t_simtime value) {
+            accumulatedValue += value;
+            valueRecords[noOfSamples++] = value;
+            if(value < minimum) minimum = value;
+            if(value > maximum) maximum = value;
+        }
+
+        t_simtime getConfidenceIntervalRange() {
+            t_simtime average = this->avg();
+            t_simtime val = 0;
+            for(int i = 0; i < noOfSamples; i++)
+                val += (valueRecords[i] - average)*(valueRecords[i] - average);
+            t_simtime estimOfVariance = val / (noOfSamples - 1);
+            return sqrt(estimOfVariance * estimOfVariance / noOfSamples);
+        }
+
+        t_simtime avg() {
+            return accumulatedValue / noOfSamples;
+        }
+
+        t_simtime getMaximum() {
+            return maximum;
+        }
+
+        t_simtime getMinimum() {
+            return minimum;
+        }
+};
+
+
+
+/** The queue that we mimic is a queue of customers, hence this class.*/
+class Customer {
+
+    public:
+        int customerId;                 /** The ids are 1,2,3,...*/
+        t_simtime arrivalTime;          /** Following are self explanatory.*/
+        t_simtime serviceTimeStartsAt;
+        t_simtime departureTime;
+
+        Customer(int customerId, t_simtime arrivalTime) {
+            this->customerId = customerId;
+            this->arrivalTime = arrivalTime;
+            this->serviceTimeStartsAt = INVALID_TIME;
+            this->departureTime = INVALID_TIME;
+        }
+
+        /** When a customer is destructed, log the arrival and departure times for further calculations.*/
+        ~Customer() {
+            avg->pushData(abs(this->serviceTimeStartsAt - this->arrivalTime));
+        }
+};
+
+/** Exponentially distributed random stream. The mean has to be provided in the constructor. here, [ mean = beta = 1 / (lambda) ] */
+class RandomStream {
+        default_random_engine dre;
+        exponential_distribution<t_simtime> distribution;
+    public:
+        inline RandomStream(t_simtime mean)    { exponential_distribution<t_simtime> temp(1.0/mean); distribution = temp; dre.seed(rand()); }
+        inline t_simtime next()                { t_simtime t = distribution(dre); return t; }
+};
+
+class Simulator {
+
+    private:
+        priority_queue<Event*, vector<Event*>, CompareEvent>  *eventQueue;
+        t_simtime simclock;
+
+        deque<Customer*> **Q;            /** the queue of customers */
+        bool *serverBusy;                /** used to check if the server is currently busy or not*/
+        Customer **customerBeingServed;  /** the customer currently being served.*/
+        int customerIdRecord;            /** used to generate the customer ids sequentially 1->2->3->...*/
+        t_simtime serviceEndsAt;         /** used to schedule next arrival, next arrival scheduled only when time complies*/
+
+        int N;                           /** number of tellers */
+
+    public:
+        RandomStream *serviceTimeStream;                            /** two exponentially distributed random stream to mimic randomness */
+        RandomStream *interArrivalTimeStream;
+
+        void scheduleEvent(Event *event);
+        void setSimulationEndTime(t_simtime endTime);
+        void run();
+        t_simtime now();
+
+        void makeServerBusy(int tellerId);                          /** pops one from Q and adds to service. Everyone thus should join Q straightway,
+                                                                    and then make a call to this if server is free. This also updates the customer's
+                                                                    service start time.*/
+        void makeServerIdle(int tellerId);                          /** makes the server idle by setting the necessary flags (server busy,
+                                                                    customer being served) and also
+                                                                    updates the customer's departure time record. if the server is free, does nothing.*/
+        int getFreeTellerId();                                      /** upon arrival, we look for a free teller. If the teller free then the customer
+                                                                    joins the service immediately. This method serves the purpose. */
+        int getShortestQuedTellerId();                              /** in case no free teller, the customer will join the teller with shortest queue.
+                                                                    this method serves this particular purpose */
+        int getJockeyableQueueId(int tellerId);                     /** in case a customer leaves a service, in particular, when a queue length at some teller
+                                                                    we will have to look around to see if is shortened, we have to look around to find
+                                                                    the leftmost Q that has a customer who can jockey. In brief, the customer at the tail
+                                                                    of the jockey-able Q will jockey. */
+        void jockey(int);                                           /** the event of jockeying is handled here separately as a non-event routine. this
+                                                                    routine is invoked from the departure event routine, where the queue from which a
+                                                                    departure has occurred is passed as the argument */
+
+        inline void addCustomerToQueue(Customer *C, int tellerId)   { Q[tellerId]->push_back(C); timeAvg[tellerId]->pushData(Q[tellerId]->size(), now()); }
+        inline bool isServerBusy(int tellerId)                      { return serverBusy[tellerId]; }
+        inline int nextCustomerId()                                 { return ++customerIdRecord; }
+        inline t_simtime serviceEndTime()                           { return serviceEndsAt; }
+
+        Simulator(int);
+        ~Simulator();
+
+};
+
+class EndEvent : public Event {
+    public:
+        EndEvent(t_simtime time, EventType type, string name);
+        EndEvent(t_simtime time);
+        void processEvent(Simulator *sim);
+};
+
+
+class ArrivalEvent : public Event {
+    public:
+        ArrivalEvent(t_simtime time, EventType type, string name);
+        ArrivalEvent(t_simtime time);
+        void processEvent(Simulator *sim);
+};
+
+class DepartureEvent : public Event {
+        int tellerId;             /** this is needed because in the event of a departure, we need to invoke jockey.
+                                      To do jockeying, we have to know which teller was serving him, therefore this.*/
+    public:
+        DepartureEvent(t_simtime time, int tellerId, EventType type, string name);
+        DepartureEvent(t_simtime time, int tellerId);
+        void processEvent(Simulator *sim);
+        inline int getTellerId() { return tellerId; }
+};
